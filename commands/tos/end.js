4a56f5d283a263b0798a673db26ee0b04240eb7e
Tiger Tian <tiantigerg888@gmail.com>
2018-08-23 17:31:35 -0400

    Add functionality for tos initialization process, prep to integrate Babel

--- commands/tos/end.js
+++ commands/tos/end.js
@@ -4,12 +4,10 @@ module.exports = {
     name: "end",
     description: 'Ends a Town Of Salem game. Only executable by moderator',
     execute(message) {
-        console.log("it's done");
         if (!game.running) return message.reply("Wow you really don't like this game.");
         if (message.author != game.moderator) return message.reply("Ask the faggot in charge");
-        if (message.channel == game.botChannel) {
-            game.botChannel.delete();
-            game.category.delete();
-        }
+        if (message.channel != game.botChannel) return message.reply('Wrong channel my dood');
+        game.botChannel.delete();
+        game.category.delete();
     }
 }
--- commands/tos/tos.js
+++ commands/tos/tos.js
@@ -1,18 +1,31 @@
 const game = require("./src/game.js");
 
+/*const initializeChannels = async function(message) {
+    message.guild.createChannel('Town Of Salem', "category")
+            .then(game.category = message.guild.channels.find(channel => channel.name == 'Town Of Salem'))
+            .then(message.guild.createChannel("gods-hand", 'text'))
+            .then(game.botChannel = message.guild.channels.find(channel => channel.name == "gods-hand"))
+            .then(message.channel.send(game.category + game.botChannel))
+            .then(console.log(game))
+            .then(game.botChannel.setParent(game.category))
+}; */
+
+const initializeGame = async function (message) {
+    game.category = await message.guild.createChannel('Town Of Salem', 'category');
+    game.botChannel = await message.guild.createChannel('gods-hand', 'text');
+    game.botChannel.setParent(game.category);
+}
+
 module.exports = {
     name: "tos",
     description: `Start a game of Town Of Salem, with you as the moderator.`,
-    async execute(message) {
+    execute(message) {
         if (game.running) return message.reply("Stop being a sore-ass loser");
         game.running = true;
         game.moderator = message.author;
         game.players.push(message.author);
+        
         //create tos category, voice channel, bot channel private mafia text channel
-        message.guild.createChannel('Town Of Salem', "category")
-            .then(game.category = message.guild.channels.find(channel => channel.name == 'Town Of Salem'))
-            .then(message.guild.createChannel("gods-hand", 'text'))
-            .then(game.botChannel = message.guild.channels.find(channel => channel.name == "gods-hand"))
-            .then(game.botChannel.setParent(game.category))
+        initializeGame(message);
     }
 }
